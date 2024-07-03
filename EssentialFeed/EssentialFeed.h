741e3b3cb9d4aa031f122ce94b566dd3dec8c8bc
Vlad <vlad@unpi.dev>
2024-07-03 07:43:49 +0300

    Add header file

---
+++ EssentialFeed/EssentialFeed.h
@@ -0,0 +1,17 @@
+//
+//  lala.h
+//  lala
+//
+//  Created by Vlad Toma on 03.07.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for lala.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for lala.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
