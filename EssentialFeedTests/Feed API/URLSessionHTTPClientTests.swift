072b96a25ec3c26a3536eb4bf38a73952d14140f
Vlad <vlad@unpi.dev>
2024-08-17 14:15:09 +0300

    Move start and stop requests

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -27,8 +27,18 @@ final class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
     
-    func test_getFromURL_performGETRequestWithURL() {
+    override func setUp() {
+        super.setUp()
         URLProtocolStub.startInterceptingRequests()
+        
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
+    func test_getFromURL_performGETRequestWithURL() {
         
         let url = URL(string: "https://any-url.com")!
         let exp = expectation(description: "Wait for expectation")
@@ -42,11 +52,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         sut.get(from: url) { _ in }
         wait(for: [exp], timeout: 1)
-        URLProtocolStub.stopInterceptingRequests()
+       
     }
     
     func test_getFromURL_failsOnRequestError() {
-        URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil,  error: error)
@@ -68,8 +77,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1)
-        URLProtocolStub.stopInterceptingRequests()
-        
     }
 }
     
