16ce13165aaeb8fd21079b7ae42c25c0c316177b
Vlad <vlad@unpi.dev>
2024-10-08 10:21:47 +0300

    Core Data Feed Store implementation

---
+++ EssentialFeed/Feed Cache/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,38 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Vlad Toma on 08.10.2024.
+//
+
+import CoreData
+
+
+import CoreData
+
+@objc(ManagedFeedImage)
+
+final class ManagedFeedImage: NSManagedObject {
+    @NSManaged internal var id: UUID
+    @NSManaged internal var imageDescription: String?
+    @NSManaged internal var location: String?
+    @NSManaged internal var url: URL
+    @NSManaged internal var cache: ManagedCache
+}
+
+extension ManagedFeedImage {
+    internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            return managed
+        })
+    }
+
+    internal var local: LocalFeedImage {
+        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+    }
+}
