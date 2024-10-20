f1227a14ad52d02ee98868bf2769d065a5b62151
Vlad <vlad@unpi.dev>
2024-10-20 18:59:04 +0300

    Prototype feed data on the table view

--- Prototype/Prototype/FeedImageCell.swift
+++ Prototype/Prototype/FeedImageCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class FeedImageCelll: UITableViewCell {
+final class FeedImageCell: UITableViewCell {
     @IBOutlet private(set) var locationContainer: UIView!
     @IBOutlet private(set) var locationLabel: UILabel!
     @IBOutlet private(set) var feedImageView: UIImageView!
