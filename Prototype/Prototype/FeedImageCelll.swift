373ec0641bc2377479e54c0057e33807d70d4837
Vlad <vlad@unpi.dev>
2024-10-20 18:41:50 +0300

    Add `FeedImageCell` with sotryboard connections

---
+++ Prototype/Prototype/FeedImageCelll.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCelll.swift
+//  Prototype
+//
+//  Created by Vlad Toma on 20.10.2024.
+//
+
+import UIKit
+
+final class FeedImageCelll: UITableViewCell {
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+    
+}
